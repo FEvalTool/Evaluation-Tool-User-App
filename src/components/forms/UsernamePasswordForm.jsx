dc8c13b2eb915b8dbe4c383491d5fbd77e0fe14a
TranAnhVu0411 <vuta183674@gmail.com>
2025-09-30 21:27:22 +0700

    refactor: separate form component from login page

---
+++ src/components/forms/UsernamePasswordForm.jsx
@@ -0,0 +1,59 @@
+import { Button, Form, Input, message } from "antd";
+
+const UsernamePasswordForm = ({ onSubmit }) => {
+    const [form] = Form.useForm();
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const onFinish = async (values) => {
+        try {
+            await onSubmit(values);
+        } catch (error) {
+            messageApi.open({
+                type: "error",
+                content: error.response.data.message,
+            });
+        }
+    };
+
+    return (
+        <Form
+            layout="vertical"
+            form={form}
+            onFinish={onFinish}
+            autoComplete="off"
+        >
+            {contextHolder}
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please input your username!",
+                    },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please input your password!",
+                    },
+                ]}
+            >
+                <Input.Password />
+            </Form.Item>
+            <Form.Item style={{ float: "right" }}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export default UsernamePasswordForm;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,81 +1,30 @@
-import { useState } from "react";
 import { useNavigate } from "react-router";
-import { Form, Input, Typography, Statistic, Button, message } from "antd";
+import { Flex, Typography, message } from "antd";
 
 import { login } from "../services/authServices";
+import UsernamePasswordForm from "../components/forms/UsernamePasswordForm";
 
-const { Title, Link } = Typography;
+const { Text, Title, Link } = Typography;
 
 const LoginPage = () => {
-    const [formDisabled, setFormDisabled] = useState(false);
     const navigate = useNavigate();
-    const [messageApi, contextHolder] = message.useMessage();
-    const [form] = Form.useForm();
 
-    const onFinish = async (values) => {
-        try {
-            setFormDisabled(true);
-            await login(values);
-            navigate("/test-main");
-        } catch (error) {
-            setFormDisabled(false);
-            messageApi.open({
-                type: "error",
-                content: error.response.data.message,
-            });
-        }
+    const onSubmitLogin = async (values) => {
+        await login(values);
+        navigate("/test-main");
     };
+
     return (
-        <Form
-            layout="vertical"
-            form={form}
-            onFinish={onFinish}
-            autoComplete="off"
-            disabled={formDisabled}
-        >
-            {contextHolder}
-            <Form.Item>
-                <Title level={3} style={{ margin: "0px" }}>
-                    Login
-                </Title>
-            </Form.Item>
-            <Form.Item
-                label="Username"
-                name="username"
-                rules={[
-                    {
-                        required: true,
-                        message: "Please input your username!",
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: "Please input your password!",
-                    },
-                ]}
-            >
-                <Input.Password />
-            </Form.Item>
-            <Form.Item>
-                <Link href="/forgot-password">Forgot Password?</Link>
-            </Form.Item>
-            <Form.Item style={{ float: "right" }}>
-                <Button
-                    type="primary"
-                    htmlType="submit"
-                    disabled={formDisabled}
-                >
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
+        <>
+            <Title level={3} style={{ margin: "0px" }}>
+                Login
+            </Title>
+            <UsernamePasswordForm onSubmit={onSubmitLogin} />
+            <Flex gap="5px">
+                <Text>Forgot</Text>
+                <Link href="/forgot-password">Username / Password ?</Link>
+            </Flex>
+        </>
     );
 };
 export default LoginPage;
