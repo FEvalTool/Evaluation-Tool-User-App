49eed39caa60627bb3211dd5b4cd0ccab0c7e52f
TranAnhVu0411 <vuta183674@gmail.com>
2026-01-11 22:52:56 +0700

    fix: fix test issue - `tip` only work in nest or fullscreen pattern.

--- src/components/PageLoading.jsx
+++ src/components/PageLoading.jsx
@@ -1,16 +1,14 @@
-import { Spin } from "antd";
+import { Spin, Flex, Typography } from "antd";
+
+const { Text } = Typography;
 
 const PageLoading = () => (
-    <div
-        style={{
-            height: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-        }}
-    >
-        <Spin size="large" tip="Loading Page..." />
-    </div>
+    <Flex vertical align="center" justify="center" style={{ height: "100vh" }}>
+        <Spin size="large" />
+        <Text style={{ marginTop: 16 }} type="secondary" strong>
+            Loading Page...
+        </Text>
+    </Flex>
 );
 
 export default PageLoading;
